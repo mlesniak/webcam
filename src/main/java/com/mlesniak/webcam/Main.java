1bd07cb7647fe4c2b96f0806946c43d4b9e3bea9
Michael Lesniak <mlesniak@micromata.de>
2016-05-15 12:03:59 +0200

    Frame processing example

--- src/main/java/com/mlesniak/webcam/Main.java
+++ src/main/java/com/mlesniak/webcam/Main.java
@@ -1,9 +1,8 @@
 package com.mlesniak.webcam;
 
-import org.bytedeco.javacv.CanvasFrame;
-import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.FrameGrabber;
-import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_imgproc;
+import org.bytedeco.javacv.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +16,8 @@ import javax.swing.*;
  */
 @SpringBootApplication
 public class Main implements CommandLineRunner {
+    private OpenCVFrameConverter.ToMat converter;
+
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Main.class);
         app.setHeadless(false);
@@ -29,10 +30,29 @@ public class Main implements CommandLineRunner {
         canvas.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         FrameGrabber grabber = new OpenCVFrameGrabber("");
         grabber.start();
+
+        converter = new OpenCVFrameConverter.ToMat();
+
         while (true) {
             Frame frame = grabber.grab();
+            frame = processFrame(frame);
             canvas.setCanvasSize(grabber.getImageWidth(), grabber.getImageHeight());
             canvas.showImage(frame);
         }
+    }
+
+    /**
+     * Playground for frame processing.
+     *
+     * @param frame src frame
+     * @return processed frame
+     */
+    private Frame processFrame(Frame frame) {
+        opencv_core.Mat mat = converter.convert(frame);
+        opencv_imgproc.rectangle(mat,
+                new opencv_core.Point(50, 50),
+                new opencv_core.Point(100, 100),
+                new opencv_core.Scalar(255, 255, 0, 1));
+        return converter.convert(mat);
     }
 }
